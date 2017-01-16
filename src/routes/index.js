6d26c0472bb4dd744deb39687097149f1a17afe6
Flavio Cassini <fbcassini@fullsail.edu>
2017-01-16 15:05:03 -0500

    v1.2.0

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,21 @@
-const user = require("../models/user");
-module.exports = (express) => {
-const router = express.Router();
+const user = require('../models/user');
+const util = require('../util');
 
-router.get("/launch/:shorturl", (req, res) => {
+module.exports = (express) => {
+  const router = express.Router();
+
+
+  router.get('/launch/:shorturl', (req, res) => {
     req.body.shorturl = req.params.shorturl;
     user.findShortURL(req.body, (err) => {
+      util.debug('Error: Someone tried to launch shorturl', err, 'Error!');
       res.status(500).json(err);
     }, (data) => {
-      res.redirect("http://www." + data.longurl);
-    })
+      util.debug('Success: Someone tried to launch short url', data, 'Success');
+      res.redirect('http://www.' + data.longurl);
+    });
   });
-router.use('/api/v1/', require('./api/users')(express));
+  router.use('/api/v1/', require('./api/users')(express));
 
-return router;
-
+  return router;
 };
